43b7a26fb77550c754d2cae2ff50f9e1a7fee5fc
Andre Madsen <83783164+MadsenAMS@users.noreply.github.com>
2021-11-30 18:24:23 -0300

    Delete Scripting directory

--- Scripting/Main Game/ScriptFinder.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-public static class ScriptFinder 
-{
-    public static Script Get<Script>()
-    {
-        return GameObject.Find("SCRIPTS").GetComponentInChildren<Script>();
-    }   
-}
